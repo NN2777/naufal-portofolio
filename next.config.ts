84170016f48c3460d693fa8da9870f76eaaa922a
Needred <naufanafidn@gmail.com>
2025-09-01 14:51:36 +0700

    deploying

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // 👈 enables static export
+  images: {
+    unoptimized: true, // 👈 disable Next.js image optimization for GitHub Pages
+  },
+  basePath: '/naufal-portofolio', // 👈 replace with your GitHub repo name
 };
 
 export default nextConfig;
