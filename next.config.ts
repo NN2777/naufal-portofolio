4e0a5b1f38172c2d63872f70a65b465ac97370f0
Needred <naufanafidn@gmail.com>
2025-09-02 10:30:47 +0700

    deploying

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,9 @@ const nextConfig: NextConfig = {
 
   output: 'export',
   eslint: { ignoreDuringBuilds: true },
-
   images: { unoptimized: true },
   basePath: "/naufal-portofolio",     // 👈 repo name
-  assetPrefix: "/naufal-portofolio/", // 👈 needed for static files
+  assetPrefix: "/naufal-portofolio", // 👈 needed for static files
   trailingSlash: true,
 
 }
